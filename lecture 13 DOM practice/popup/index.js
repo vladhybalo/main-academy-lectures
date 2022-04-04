53b520d9d87e8822912b8344055b8b228c3e13f6
unknown <vladhybalo@gmail.com>
2022-04-04 20:52:29 +0300

    add 13 lecture

---
+++ lecture 13 DOM practice/popup/index.js
@@ -0,0 +1,7 @@
+const showPopup = () => {
+    document.querySelector('.popup').classList.remove('hide');
+}
+
+const hidePopup = () => {
+    document.querySelector('.popup').classList.add('hide');
+}
